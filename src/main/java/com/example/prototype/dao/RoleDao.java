fc8c09967a1f9d43a05fc9de9d71852ee86041cb
Tn-ko <99136238+Tn-ko@users.noreply.github.com>
2023-02-14 12:04:44 +0300

    Update RoleDao.java

--- src/main/java/com/example/prototype/dao/RoleDao.java
+++ src/main/java/com/example/prototype/dao/RoleDao.java
@@ -2,7 +2,7 @@ package com.example.prototype.dao;
 
 import com.example.prototype.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
-// dao interface for Role
+// dao interface to Role
 public interface RoleDao extends JpaRepository<Role, Long> {
 
 
